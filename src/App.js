61ed12fab3fad6cbcefcbc074f5106f2f39bf2f1
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-07-11 23:34:39 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from 'react-router-dom'
+import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import { MdFastfood } from 'react-icons/md';
+import './index.css';
+
+function App() {
+  return (
+    // className="card" makes the image zoom in on hover. It's located in index.css
+    <div className="App">
+    <BrowserRouter>
+    <Nav>
+    <MdFastfood id="yummyLogo"/>
+      <Logo to={'/'}> Yummy</Logo>
+    </Nav>
+      <Search />
+      <Category />
+      <Pages />
+    </BrowserRouter>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  padding: 1rem;
+  font-size: 3rem;
+  font-weight: 400;
+  font-family: 'Lobster Two';
+  background: -webkit-linear-gradient(#f27121,#e94057);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+`
+const Nav = styled.div`
+  padding: 3rem 3rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 3rem;
+  }
+`
+
+export default App;
