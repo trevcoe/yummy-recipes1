b19edee342afe74a644b4d2f9b129bf25bc41c79
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-07-14 17:21:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     <Nav>
     <a href='/'>
     <MdFastfood id="yummyLogo"/>
-      <Logo> Yummy</Logo>
-    </a>
+     </a>
+      <Logo to={'/'}> Yummy</Logo>
     </Nav>
       <Search />
       <Category />
